4b2c5954d6f8510862f16588349bf887561a065e
Jonas Lundberg <jonasl@catchmecode.com>
2017-08-16 11:02:15 +0200

    Add vertical ScrollView content offset on Android
    This is the (evil) twin commit to:
    https://github.com/facebook/react-native/pull/12502
    
    Fixes that contentOffset is not available on
    Vertical scroll-views.
    
    Verification:
    * Create an Android component with a ScrollView. Add enough
      items to fill beyond the visible screen. Set the content
      offset to some value that makes the content scroll. Verify
      that it snaps to the y-axis as expected.
    * Remove items so screen is no longer filled, keep content
      offset parameter. Scroll view still works but does not scroll.
    * Set only one parameter to content-offset (x || y) -
      parameters are now ignored since they must be a pair.
    * Remove contentOffset, scrollView still works as before.

--- ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/scroll/ReactScrollView.java
@@ -8,6 +8,11 @@
 package com.facebook.react.views.scroll;
 
 import android.annotation.TargetApi;
+import javax.annotation.Nullable;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -61,6 +66,8 @@ public class ReactScrollView extends ScrollView implements ReactClippingViewGrou
   private int mEndFillColor = Color.TRANSPARENT;
   private View mContentView;
   private ReactViewBackgroundManager mReactBackgroundManager;
+  private @Nullable ReactViewBackgroundDrawable mReactBackgroundDrawable;
+  private HashMap<String, Integer> mContentOffset = null;
 
   public ReactScrollView(ReactContext context) {
     this(context, null);
@@ -140,10 +147,21 @@ public class ReactScrollView extends ScrollView implements ReactClippingViewGrou
         MeasureSpec.getSize(heightMeasureSpec));
   }
 
+  public void setContentOffset(HashMap<String, Integer> contentOffset) {
+    mContentOffset = contentOffset;
+  }
+
+
   @Override
   protected void onLayout(boolean changed, int l, int t, int r, int b) {
     // Call with the present values in order to re-layout if necessary
-    scrollTo(getScrollX(), getScrollY());
+    // If contentOffset is set scroll to its position
+    if (mContentOffset != null) {
+      scrollTo(mContentOffset.get("x"), mContentOffset.get("y"));
+    } else {
+      // Call with the present values in order to re-layout if necessary
+      scrollTo(getScrollX(), getScrollY());
+    }
   }
 
   @Override
